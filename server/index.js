5b2f7e9af8cb6b7f42298cd4ae06191d31a86982
sarthakgawri889 <144597233+sarthakgawri889@users.noreply.github.com>
2024-05-29 08:19:53 +0530

    minor change in index

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import Connection from './database/db.js';
 import route from './routes/route.js';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-
+import lobbyRoute from "./routes/lobby.route.js";
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -22,7 +22,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // Define your routes
 app.use('/', route);
+app.use("/api/lobbies", lobbyRoute);
 
+app.get("/", (req, res) => {
+    //server has sent the response
+    res.send("Hello form Node API Server");
+  });
+  
 // Connect to the database
 Connection();
 
